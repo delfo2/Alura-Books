01475f26f187d384e1cd945c78099509d0ea1f14
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2022-12-08 12:44:20 -0300

    acrescentando arquivo js e fazendo logica basica de request para API viaCEP

---
+++ js/script.js
@@ -0,0 +1,13 @@
+async function buscaEndereco (cep) {
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let cepCorrigido = await consultaCEP.json();
+        if(cepCorrigido.erro) {
+            throw Error ('CEEEEEEEP errrrrrrrrrrrrrrrrrrrrrrado kkkkkkkk!!!!!!!!!!! LOL OMG ajkfdhajshfja');
+        } else {
+            console.log(cepCorrigido);
+        }
+    } catch (erro) {
+        console.log(erro);
+    }
+}
